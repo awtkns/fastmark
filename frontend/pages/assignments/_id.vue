f270d72260c91d21f1ca2d40aaeb4b12afdbdb40
Adam <cadamrun@gmail.com>
2020-09-20 12:19:32 -0700

    missing file labels

--- frontend/pages/assignments/_id.vue
+++ frontend/pages/assignments/_id.vue
@@ -8,15 +8,15 @@
             <v-spacer/>
             <v-chip class="mr-2" v-if="s.late" color="error" outlined v-text="'Late'" />
             <v-chip class="mr-2" v-if="s.overdue" color="error" v-text="'overdue'" />
+            <v-chip class="mr-2" v-if="getMissing(assignment.expected_files, s.files).length" color="warning" v-text="'Missing files'" outlined />
           </v-row>
 
         </v-expansion-panel-header>
         <v-expansion-panel-content>
-          Some content
+          <v-chip dark v-for="f in s.files" v-text="f.filename" class="mx-1"/>
         </v-expansion-panel-content>
       </v-expansion-panel>
     </v-expansion-panels>
-    {{ assignment }}
   </div>
 </template>
 
@@ -29,10 +29,16 @@ export default {
     panel: undefined,
   }),
   computed: {
-    ...mapState(['assignment'])
+    ...mapState(['assignment']),
   },
   created() {
     this.$store.dispatch('fetchAssignment', this.$route.params.id)
+  },
+  methods: {
+    getMissing: (expected, files) => {
+      const actual = files.map(f => f.filename)
+      return expected.filter(e => !actual.includes(e))
+    }
   }
 }
 </script>
