d16a02aceecfabe67f8f04afa4b5c3a85f5d7055
Adam <cadamrun@gmail.com>
2020-09-20 00:06:53 -0700

    simple assignment vue

--- frontend/store/index.js
+++ frontend/store/index.js
@@ -2,13 +2,15 @@ import { generic_get, generic_post } from "~/api";
 import { uploadAssignmentFile } from "~/api/assignments"
 
 export const state = () => ({
-  assignments: []
+  assignments: [],
+  assignment: undefined
 })
 
 
 export const mutations = {
   setAssignments: (state, assignments) => state.assignments = assignments,
-  addAssignment: (state, assignment) => state.assignments.push(assignment)
+  setAssignment: (state, assignment) => state.assignment = assignment,
+  addAssignment: (state, assignment) => state.assignments.push(assignment),
 }
 
 export const actions = {
@@ -19,5 +21,8 @@ export const actions = {
     const db_assignment =  await generic_post(this, '/assignments/', assignment)
     uploadAssignmentFile(this, file, db_assignment)
     commit('addAssignment', db_assignment)
-  }
+  },
+  async fetchAssignment({ commit }, id) {
+    commit('setAssignment', await generic_get(this, `assignments/${id}/`))
+  },
 }
