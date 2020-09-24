54ccea3fda3125dc144a784f8849c17f0b3cd981
Adam <cadamrun@gmail.com>
2020-09-24 00:57:59 -0700

    added breadcrumbs

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -2,7 +2,14 @@
   <v-app>
     <v-app-bar color="deep-purple accent-4" dense dark app>
       <v-app-bar-nav-icon @click="nav = !nav"/>
-      <span class="title">FastMark</span>
+      <div class="title">FastMark</div>
+      <v-breadcrumbs :items="crumbs">
+        <template v-slot:item="{ item }">
+          <v-breadcrumbs-item :disabled="item.disabled" :to="item.to" style="color: white; text-decoration: none">
+            {{ item.text.toUpperCase() }}
+          </v-breadcrumbs-item>
+        </template>
+      </v-breadcrumbs>
     </v-app-bar>
 
     <v-navigation-drawer v-model="nav" app>
@@ -29,16 +36,15 @@
 
       <template v-slot:append>
         <div class="pa-2">
-          <f-assignment />
+          <f-assignment/>
         </div>
       </template>
     </v-navigation-drawer>
 
 
-
     <v-main>
       <v-container>
-        <nuxt />
+        <nuxt/>
       </v-container>
     </v-main>
 
@@ -46,19 +52,44 @@
 </template>
 
 <script>
-import { mapState } from 'vuex'
-import FAssignment from "../components/f-assignment";
+  import {mapState} from 'vuex'
+  import FAssignment from "../components/f-assignment";
 
-export default {
-  components: {FAssignment},
-  data: () => ({
-    nav: false
-  }),
-  computed: {
-    ...mapState(['assignments'])
-  },
-  created() {
-    this.$store.dispatch('fetchAssignments')
-  },
-}
+  export default {
+    components: {FAssignment},
+    data: () => ({
+      nav: false
+    }),
+    computed: {
+      ...mapState(['assignments']),
+      crumbs: ctx => {
+        if (ctx.$route.path === '/') return [
+          {
+            to: '/',
+            text: 'Dashboard'
+          }
+        ]
+
+        const routes = ctx.$route.path.split('/');
+        const items = []
+        for (const i in routes) {
+          items.push({
+            to: items.length ? items[items.length - 1].to + `${routes[i]}/` : '/',
+            text: routes[i] ? routes[i] : 'Dashboard'
+          })
+        }
+
+        return items
+      }
+    },
+    created() {
+      this.$store.dispatch('fetchAssignments')
+    },
+  }
 </script>
+<style>
+a, a:hover, a:focus, a:active {
+    text-decoration: none;
+    color: inherit !important;
+}
+</style>
