1e3357fa7a9dd26f18b125c316daa9fb91e22939
Adam <cadamrun@gmail.com>
2020-09-23 23:47:42 -0700

    active jobs table

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,5 +1,14 @@
 <template>
-  <v-row justify="center" align="center">
-    AUTO MARKER
+  <v-row>
+    <v-col cols="12" class="headline">Active Jobs</v-col>
+    <v-col cols="12" class="headline">
+      <t-active-jobs />
+    </v-col>
   </v-row>
 </template>
+<script>
+import TActiveJobs from "../components/t-active-jobs";
+export default {
+  components: {TActiveJobs}
+}
+</script>
