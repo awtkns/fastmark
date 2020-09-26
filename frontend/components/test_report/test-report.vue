81d63fa8139cdea2e908da14702dcb1f40cb48b7
Adam <cadamrun@gmail.com>
2020-09-26 15:21:19 -0700

    result page

---
+++ frontend/components/test_report/test-report.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-expansion-panels flat>
+    <v-expansion-panel v-if="report" v-model="panel" class="pa-0">
+      <v-expansion-panel-header :color="color" class="subtitle-1l">
+        <span class="title">{{ report.name }}</span>{{ report.tests - errorCount }}/{{ report.tests }}
+      </v-expansion-panel-header>
+      <v-expansion-panel-content class="test-result-panel">
+        <test-report v-for="r in report.testsuites" :report="r" class="mt-2 px-2" />
+        <v-sheet v-for="test in report.testsuite" :color="test.result === 'COMPLETED' ? 'success lighten-1' : 'error lighten-1'">
+          <v-row no-gutters class="mx-1 text-caption" >[{{ test.status}}] {{ test.name }}<v-spacer />{{test.result}}</v-row>
+        </v-sheet>
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+  </v-expansion-panels>
+</template>
+
+<script>
+  export default {
+    name: "test-report",
+    data: () => ({
+      panel: false
+    }),
+    props: {
+      report: undefined
+    },
+    computed: {
+      errorCount: ctx => ctx.report.errors + ctx.report.failures,
+      success: ctx => ctx.errorCount !== 0,
+      color: ctx => ctx.errors ? 'error' : 'success',
+    }
+  }
+</script>
+<style>
+.test-result-panel > div {
+  padding: 0 !important;
+}
+</style>
