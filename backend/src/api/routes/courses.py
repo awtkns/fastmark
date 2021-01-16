37c7d4a8b93f62ebf260712a3350a0fa1bc8cb28
Adam <cadamrun@gmail.com>
2021-01-16 10:58:46 -0800

    :wastebasket: cleaning up

--- backend/src/api/routes/courses.py
+++ backend/src/api/routes/courses.py
@@ -43,40 +43,51 @@ async def create_upload_file(file: bytes = File(...), filename: str = Form(...),
     for file in os.listdir(assignment_folder):
         print(file)
 
-        zip_path = os.path.join(assignment_folder, file)
+        submission_file_path = os.path.join(assignment_folder, file)
 
         if os.path.isdir(file):
             break
 
-        # Students Folders
+        try:
+            d2l_id, name, submission_datetime, file_name, *_ = file.split(' - ')
+        except ValueError:
+            break
+
+        submission_datetime = datetime.strptime(submission_datetime, '%b %d, %Y %I%M %p')
+        if not (student := db.query(models.Student).filter_by(d2l_id=d2l_id).first()):
+            student = models.Student(d2l_id=d2l_id, name=name).save(db)
+
+        db_submission = models.Submission(
+            assignment_id=assignment_id,
+            student_id=student.id,
+            submission_datetime=submission_datetime,
+        ).save(db)
+
+        os.mkdir(submission_folder := db_submission.path)
+
+        # Students submitted a zip
         if file.endswith('.zip'):
-            d2l_id, name, submission_datetime, *_ = file.split(' - ')
-            submission_datetime = datetime.strptime(submission_datetime, '%b %d, %Y %I%M %p')
 
-            if not (student := db.query(models.Student).filter_by(d2l_id=d2l_id).first()):
-                student = models.Student(d2l_id=d2l_id, name=name).save(db)
-
-            db_submission = models.Submission(
-                assignment_id=assignment_id,
-                student_id=student.id,
-                submission_datetime=submission_datetime,
-            ).save(db)
-
-            os.mkdir(submission_folder := db_submission.path)
-            shutil.unpack_archive(zip_path, extract_dir=submission_folder)
+            shutil.unpack_archive(submission_file_path, extract_dir=submission_folder)
             utils.flatten_dir(submission_folder)
 
             # Cleaning up
             [os.rmdir(dir_) for dir_ in os.listdir(submission_folder) if os.path.isdir(dir_)]
 
-            [models.SubmissionFile(
-                submission_id=db_submission.id,
-                filename=f,
-                path=os.path.relpath(p, config.UPLOAD_DIR)
-            ).save(db) for f in os.listdir(submission_folder) if not delete_makefile(p := os.path.join(submission_folder, f))]
+        # student submitted a single file
+        else:
+            shutil.copyfile(submission_file_path, os.path.join(submission_folder, file_name))
+
+        # Saving files
+        [models.SubmissionFile(
+            submission_id=db_submission.id,
+            filename=f,
+            path=os.path.relpath(p, config.UPLOAD_DIR)
+        ).save(db) for f in os.listdir(submission_folder) if
+         not delete_makefile(p := os.path.join(submission_folder, f))]
 
         try:
-            os.remove(zip_path)
+            os.remove(submission_file_path)
         except PermissionError:
             pass
 
